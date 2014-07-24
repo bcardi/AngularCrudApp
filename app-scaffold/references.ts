f6988b9c3936fc98ee34b2e1bd77cfb277137f03
Bob Cardenas <bob.cardenas@fisglobal.com>
2014-07-24 00:31:03 -0500

    Delete app-scaffold
    Now that we have a yeoman generator, we no longer need app-scaffold

--- app-scaffold/references.ts
+++
@@ -1,2 +0,0 @@
-///<reference path='../components/angular-crud/angular-crud.d.ts' />
-///<reference path='../components/angular-crud/resource-service.d.ts' />
--- app-scaffold/resource/index.ts
+++
@@ -1,5 +0,0 @@
-///<reference path='module.ts' />
-///<reference path='resource-service.ts' />
-///<reference path='list-controller.ts' />
-///<reference path='new-controller.ts' />
-///<reference path='edit-controller.ts' />
